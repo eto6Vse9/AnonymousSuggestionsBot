7380a8096a3f63a3655b38e03deee100b06f69f2
eto6Vse9 <dolly-zap-retail@duck.com>
2025-01-31 13:19:30 +0200

    ok

--- src/main/java/ru/mk3/suggestions/SuggestionsBot.java
+++ src/main/java/ru/mk3/suggestions/SuggestionsBot.java
@@ -112,7 +112,6 @@ public class SuggestionsBot extends TelegramLongPollingBot {
                 sendMediaGroup.setMedias(mediaGroup);
 
                 for (String botAdminId : BotConfig.BOT_ADMINS) {
-                    System.out.println("send " + botAdminId);
                     sendMediaGroup.setChatId(botAdminId);
                     send(sendMediaGroup);
                 }
@@ -163,7 +162,6 @@ public class SuggestionsBot extends TelegramLongPollingBot {
         copyMessage.setMessageId(message.getMessageId());
 
         for (String botAdminId : BotConfig.BOT_ADMINS) {
-            System.out.println("send " + botAdminId);
             copyMessage.setChatId(botAdminId);
             send(copyMessage);
         }
